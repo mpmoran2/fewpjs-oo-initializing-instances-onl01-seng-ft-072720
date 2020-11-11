20bedfa19552a12fea61bcddd173e83e296de7c9
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-11-11 17:01:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code here
+
+//Breakfast
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food
+    this.drink = drink
+  }
+}
+
+//Lunch
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad
+    this.soup = soup
+    this.drink = drink
+  }
+}
+
+//Dinner
+class Dinner {
+  constructor(salad, soup, entree, dessert) {
+    this.salad = salad 
+    this.soup = soup 
+    this.entree = entree 
+    this._dessert = dessert
+  }
+}
