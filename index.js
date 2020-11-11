4518ec7fd17a64ef3ebd517b160f7752756b435a
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-11-11 17:00:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,24 +8,6 @@ class Breakfast {
   }
 }
 
-class Lunch {
-  constructor(salad, soup, drink) {
-    this.salad = salad
-    this.soup = soup
-    this.drink = drink
-  }
-}
-
-class Dinner {
-  constructor(salad, soup, entree, dessert) {
-    this.salad = salad 
-    this.soup = soup 
-    this.entree = entree 
-    this._dessert = dessert
-  }
-}
-
-
 //Lunch
 
 
